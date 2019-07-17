463592900f6d7de2a72039f1506168612ab72477
Amith Sai <47681946+amithbs1@users.noreply.github.com>
2019-07-17 22:06:41 +0530

    updated root check java

--- android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
+++ android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import com.scottyab.rootbeer.RootBeer;
 import android.os.Build;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class RootedCheck {
 
     private static final String ONEPLUS = "oneplus";
@@ -17,14 +21,17 @@ public class RootedCheck {
      * @return <code>true</code> if the device is rooted, <code>false</code> otherwise.
      */
     public static boolean isJailBroken(Context context) {
-        CheckApiVersion check;
+        // CheckApiVersion check;
 
-        if (android.os.Build.VERSION.SDK_INT >= 23) {
-            check = new GreaterThan23();
-        } else {
-            check = new LessThan23();
-        }
-        return check.checkRooted() || rootBeerCheck(context);
+        // if (android.os.Build.VERSION.SDK_INT >= 23) {
+        //     check = new GreaterThan23();
+        // } else {
+        //     check = new LessThan23();
+        // }
+        // return check.checkRooted() || rootBeerCheck(context);
+        
+         // get from build info
+        return checkRootMethod1() || checkRootMethod2() || checkRootMethod3();
     }
 
     private static boolean rootBeerCheck(Context context) {
@@ -37,5 +44,33 @@ public class RootedCheck {
             rv = rootBeer.isRooted();
         }
         return rv;
+    }
+
+    private static boolean checkRootMethod1() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private static boolean checkRootMethod2() {
+        String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"};
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkRootMethod3() {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            if (in.readLine() != null) return true;
+            return false;
+        } catch (Throwable t) {
+            return false;
+        } finally {
+            if (process != null) process.destroy();
+        }
     }
 }
